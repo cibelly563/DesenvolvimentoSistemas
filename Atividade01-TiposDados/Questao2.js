159a4e7f4be41e2886d98fd59bd64b8ddeff25df
cibelly563 <cibellyandrielly34@gmail.com>
2025-03-21 16:49:48 +0000

    questao2

---
+++ Atividade01-TiposDados/Questao2.js
@@ -0,0 +1,46 @@
+let n1=0;
+console.log(typeof(n1));
+let n2 = 3.14;
+console.log(typeof(n2));
+let n3= Infinity;
+console.log(typeof(n3));
+let n4 = NaN;
+console.log(typeof(n4));
+let n5 = true;
+console.log(typeof(n5));
+let n6 = 4>1;
+console.log(typeof(n6));
+let n7 = "texto";
+console.log(typeof(n7));
+let n8 = "é letra" + 1994;
+console.log(typeof(n8));
+let n9 = Symbol("id");
+console.log(typeof(n9));
+let n10 = Date;
+console.log(typeof(n10));
+let n11 = String;
+console.log(typeof(n11));
+let n12 = [1,2,3,4];
+console.log(typeof(n12));
+let n13 = null;
+console.log(typeof(n13));
+let n14 = undefined;
+console.log(typeof(n14));
+// let n15 = alert;
+console.log(typeof(alert));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
