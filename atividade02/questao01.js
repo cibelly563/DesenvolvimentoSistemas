36915d339696c581fa9477475980ae04a5d58562
cibelly563 <cibellyandrielly34@gmail.com>
2025-05-08 00:38:28 +0000

    atividadefabricyo

---
+++ atividade02/questao01.js
@@ -0,0 +1,20 @@
+let nota = 6;
+let nota2 = 5;
+let nota3 = 7;
+let nota4 = 4;
+
+let media = (nota+nota2+nota3+nota4)/4;
+
+if(media >= 60){
+console.log("aprovado, média", media);
+}
+
+ else if (media < 40){
+console.log("reprovado");
+}
+
+
+else{
+    console.log("em recuperaçao");
+    
+}
