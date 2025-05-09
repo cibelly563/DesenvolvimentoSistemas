ed8b854c0b233f0a574a0d0fac9d4987720ee876
cibelly563 <cibellyandrielly34@gmail.com>
2025-05-09 12:52:32 +0000

    lista de exercicios

--- Aula01/lista.js/exercicio03.js
+++ Aula01/lista.js/exercicio03.js
@@ -1,3 +1,18 @@
 /**
  * exercicio 03) faça um programa que leia 3 números e mostre se o valor da soma é par ou impar
  */
+let n1 = 2;
+let n2 = 3;
+let n3 = 4;
+let soma 
+
+soma = n1+n2+n3
+
+if(soma%2==0){
+    console.log('o numero da soma é par');
+    
+}
+else{
+    console.log('o numero da soma é impar');
+    
+}
--- Aula01/lista.js/exercicio04.js
+++ Aula01/lista.js/exercicio04.js
@@ -1,3 +1,14 @@
 /**
  * faça um programa que leia um número e mostre se ele é positivo ou negativo
  */
+
+let n1 = 0
+
+if(n1>=0){
+    console.log('é positivo');
+    
+}
+else{
+    console.log('é negativo');
+    
+}
