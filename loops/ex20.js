15b774f31bc11e651088dd0ae2d23591ac9b290c
cibelly563 <cibellyandrielly34@gmail.com>
2025-06-04 00:08:40 +0000

    lista03fabricyo

---
+++ loops/ex20.js
@@ -0,0 +1,14 @@
+let numero = 17; // Número fixo
+let divisores = 0;
+
+for (let i = 1; i <= numero; i++) {
+  if (numero % i === 0) {
+    divisores++;
+  }
+}
+
+if (divisores === 2) {
+  console.log(numero + " é um número primo.");
+} else {
+  console.log(numero + " não é um número primo.");
+}
