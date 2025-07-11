80d300bc7b81fea3d65ca8afe7874168b76dede0
cibelly563 <cibellyandrielly34@gmail.com>
2025-07-11 11:28:24 +0000

    arrayex

---
+++ array/ex01.js
@@ -0,0 +1,23 @@
+let frutas = ["maça","kiwi","morango","abacaxi"];
+ 
+//  console.log('tamanho do array:' + frutas.length);
+ 
+// console.log('array original:'+ frutas);
+
+// frutas.push("uva");
+// console.log(frutas);
+
+// frutas.push("banana");
+// console.log(frutas);
+// console.log(frutas.length);
+
+frutas[2] = "cajú";
+frutas[4] = "pera";
+frutas.push("seriguela");
+frutas[5] = "romã";
+console.log(frutas.sort());
+frutas.pop();
+console.log(frutas);
+
+
+
---
+++ array/ex02.js
@@ -0,0 +1,22 @@
+let carros = [];
+// adicionando elementos no array
+carros.push('corolla');
+carros.push('sw4');
+carros.push('onix');
+carros.push('fusca');
+//alterando elementos do array
+carros[2] = "amarok";
+//removendo o ultimo elemento
+carros.pop();
+//adicionando mas elementos no array
+carros.push('l200');
+carros.push('a3');
+carros.push('uno');
+//pecorrendo o array
+for(let i = 0; i< carros.length; i++ ){
+    console.log("indice" + i);
+    console.log("valor" + carros[i]);
+    console.log();
+    
+}
+
